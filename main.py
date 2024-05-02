41129ab85620a1597bc04b4a37e7870fe6e0cf74
arose510 <98431893+arose510@users.noreply.github.com>
2024-05-02 14:02:25 -0700

    Update Auth

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import redirect, url_for
 from website import create_app
+from website.auth import auth  # Add this line to import the auth blueprint
 
 # Create the Flask application instance using create_app function from website module
 app = create_app()
+
+# Register the auth blueprint
+app.register_blueprint(auth)
 
 # Route to handle the authentication callback from Microsoft
 @app.route('/getAToken')
